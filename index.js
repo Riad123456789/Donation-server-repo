efa50edebdc93d9604dcc1e0e329b233be6b9bd7
Riad123456789 <riad80717@gmail.com>
2023-11-05 12:44:31 +0600

    foodcollection data insert in data base are done

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// foodServerDB
-// 3Q1sY1r8XSIXOVDR
+// riad80717
+// T1VZ2SS8FdPSufQc
 
 
 
-const uri = "mongodb+srv://foodServerDB:3Q1sY1r8XSIXOVDR@cluster0.kadilur.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = "mongodb+srv://riad80717:T1VZ2SS8FdPSufQc@cluster0.vifd4px.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,20 +28,39 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const FoodCollection = client.db("FoodDB").collection("Featured Foods");
 
 
-
-    app.get("/api/v1/services", async (req, res) => {
-      res.send(' running')
+    app.get("/api/v1/FeaturedFoods", async (req, res) => {
+      const cursor = FoodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
 
     })
 
 
 
+    app.post('/AddFood', async (req, res) => {
+      const AddFood = req.body;
+
+      console.log(AddFood)
+      const result = await FoodCollection.insertOne(AddFood);
+      res.send(result)
+     
+    })
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
